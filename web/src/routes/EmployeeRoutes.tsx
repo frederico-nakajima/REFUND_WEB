c3f1748d7702a7aabc26b774706a66a543e1d739
frederico nakajima <fredericonakajimanakajima@gmail.com>
2025-08-11 21:25:27 -0300

    first commit

---
+++ web/src/routes/EmployeeRoutes.tsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from "react-router"
+
+import { Refund } from "../pages/Refund"
+
+import { Confirm } from "../pages/Confirm"
+import { NotFound } from "../pages/NotFound"
+import { AppLayout } from "../components/AppLayout"
+
+
+export function EmployeeRoutes(){
+    return(
+        <Routes>            
+            <Route path="/" element={<AppLayout/>}>
+                <Route path="/" element={<Refund/>}/>
+                <Route path="/confirm" element = {<Confirm/>}/>
+            </Route>
+
+            
+        <Route path="*" element={<NotFound/>}/>
+        </Routes>
+    )
+}
